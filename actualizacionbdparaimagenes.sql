c819649a82ac0d3baeb9b2cb3416f6a89ef56218
mariecampos <mariecamposm@gmail.com>
2026-05-14 14:05:17 -0500

    Update product images in actualizacionbdparaimagenes.sql

---
+++ actualizacionbdparaimagenes.sql
@@ -0,0 +1,57 @@
+USE techstore_db;
+
+UPDATE productos SET imagen='ryzen9.jpg' WHERE id=1;
+UPDATE productos SET imagen='rtx4090.jpg' WHERE id=2;
+UPDATE productos SET imagen='teclado.jpg' WHERE id=3;
+UPDATE productos SET imagen='Mouse Logitech G Pro X.jpg' WHERE id=4;
+UPDATE productos SET imagen='ram.jpg' WHERE id=5;
+UPDATE productos SET imagen='ssd.jpg' WHERE id=6;
+UPDATE productos SET imagen='pcgamer.jpg' WHERE id=7;
+
+UPDATE productos SET imagen='i9.jpg' WHERE id=8;
+UPDATE productos SET imagen='i7.jpg' WHERE id=9;
+UPDATE productos SET imagen='i5.jpg' WHERE id=10;
+UPDATE productos SET imagen='i3.jpg' WHERE id=11;
+UPDATE productos SET imagen='i5f.jpg' WHERE id=12;
+
+UPDATE productos SET imagen='r9.jpg' WHERE id=13;
+UPDATE productos SET imagen='r7.jpg' WHERE id=14;
+UPDATE productos SET imagen='r5.jpg' WHERE id=15;
+UPDATE productos SET imagen='r7g.png' WHERE id=16;
+UPDATE productos SET imagen='r5g.jpg' WHERE id=17;
+
+UPDATE productos SET imagen='rtx4080.jpg' WHERE id=18;
+UPDATE productos SET imagen='rtx4070.jpg' WHERE id=19;
+UPDATE productos SET imagen='rtx4060.jpg' WHERE id=20;
+UPDATE productos SET imagen='rtx3060.jpg' WHERE id=21;
+UPDATE productos SET imagen='rtx4090rog.jpg' WHERE id=22;
+
+UPDATE productos SET imagen='rx7800.jpg' WHERE id=24;
+UPDATE productos SET imagen='rx7700.jpg' WHERE id=25;
+UPDATE productos SET imagen='rx7600.jpg' WHERE id=26;
+UPDATE productos SET imagen='rx6600.jpg' WHERE id=27;
+
+UPDATE productos SET imagen='razer.jpg' WHERE id=28;
+UPDATE productos SET imagen='corsair.jpg' WHERE id=29;
+UPDATE productos SET imagen='logitech.jpg' WHERE id=30;
+UPDATE productos SET imagen='hyperx.jpg' WHERE id=31;
+UPDATE productos SET imagen='teclado.jpg' WHERE id=32;
+
+UPDATE productos SET imagen='superlight.jpg' WHERE id=33;
+UPDATE productos SET imagen='deathadder.jpg' WHERE id=34;
+UPDATE productos SET imagen='glorious.jpg' WHERE id=35;
+UPDATE productos SET imagen='zowie.jpg' WHERE id=36;
+UPDATE productos SET imagen='redragon.jpg' WHERE id=37;
+
+UPDATE productos SET imagen='sn850x.jpg' WHERE id=49;
+UPDATE productos SET imagen='crucialp3.jpg' WHERE id=50;
+UPDATE productos SET imagen='kingstonnv2.jpg' WHERE id=51;
+UPDATE productos SET imagen='mp44.jpg' WHERE id=52;
+UPDATE productos SET imagen='mx500.jpg' WHERE id=53;
+UPDATE productos SET imagen='evo870.jpg' WHERE id=54;
+UPDATE productos SET imagen='a400.jpg' WHERE id=55;
+UPDATE productos SET imagen='sa510.jpg' WHERE id=56;
+UPDATE productos SET imagen='gx2.jpg' WHERE id=57;
+
+UPDATE productos SET imagen='ramddr4.jpg' WHERE id=58;
+UPDATE productos SET imagen='ramddr5.jpg' WHERE id=59;
